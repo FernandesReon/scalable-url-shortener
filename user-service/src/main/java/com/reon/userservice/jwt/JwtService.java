7b1e7a08c5f1a6cc34a1e733195e0d34a0d80259
Reon Fernandes <reondias@gmail.com>
2026-04-07 13:13:12 +0530

    removed unwanted code from jwtservice
    logic such as 1. jwtfromheader, 2. isJwtTokenValid, 3. getUsernameFromToken are now handled at api-gateway

--- user-service/src/main/java/com/reon/userservice/jwt/JwtService.java
+++ user-service/src/main/java/com/reon/userservice/jwt/JwtService.java
@@ -2,17 +2,13 @@ package com.reon.userservice.jwt;
 
 import com.reon.userservice.model.User;
 import com.reon.userservice.model.type.Tier;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -25,35 +21,15 @@ public class JwtService {
     private final String secretKey;
     private final Long expirationTime;
     private final String issuer;
-    private final String cookieName;
 
     public JwtService(
             @Value("${security.jwt.secret-key}") String secretKey,
             @Value("${security.jwt.expiration-time}") Long expirationTime,
-            @Value("${security.jwt.issuer}") String issuer,
-            @Value("${security.cookie.name}") String cookieName
+            @Value("${security.jwt.issuer}") String issuer
     ) {
         this.secretKey = secretKey;
         this.expirationTime = expirationTime;
         this.issuer = issuer;
-        this.cookieName = cookieName;
-    }
-
-    public String getJwtFromHeader(HttpServletRequest request) {
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            return authHeader.substring(7);
-        }
-
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if (cookie.getName().equals(cookieName)) {
-                    return cookie.getValue();
-                }
-            }
-        }
-
-        return null;
     }
 
     public String generateToken(User user) {
@@ -87,26 +63,5 @@ public class JwtService {
 
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
-    }
-
-    public String getUsernameFromToken(String token) {
-        return Jwts.parser()
-                .verifyWith((SecretKey) key())
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .getSubject();
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser()
-                    .verifyWith((SecretKey) key())
-                    .build()
-                    .parseSignedClaims(token);
-            return true;
-        } catch (JwtException | IllegalArgumentException e) {
-            return false;
-        }
     }
 }
